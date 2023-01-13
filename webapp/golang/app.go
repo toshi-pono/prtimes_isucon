89fde8b815751b1b516bcc8c600de6ddb9723771
logica <84759675+logica0419@users.noreply.github.com>
2023-01-13 11:27:20 +0900

    put logger

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -19,6 +19,7 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 	gsm "github.com/bradleypeabody/gorilla-sessions-memcache"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
@@ -826,6 +827,7 @@ func main() {
 	defer db.Close()
 
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
 	r.Get("/initialize", getInitialize)
 	r.Get("/login", getLogin)
